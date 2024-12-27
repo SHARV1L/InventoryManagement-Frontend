e20ba34c7ebb420f55a03b66411f38c6a72cd993
SHARV1L <livrahs@hotmail.com>
2024-12-26 20:39:24 -0500

    Configured Landing Page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,11 +1,17 @@
 import React from "react";
+import "../styles/LandingPage.css";
 
-function  LandingPage() {
+function  LandingPage({onLogin, onSignup}) {
     return ( 
-        <div>
-        <h1>Inventory Management</h1>
-        <p>Welcome to Inventory management</p>
-        <footer>© 2024 Inventory App</footer>
+        <div className="landing-page">
+            <header className="header">
+                <h1>Inventory Management</h1>
+                <p>Keep track of your inventory efficiently and never miss an expiry date!</p>
+            </header>
+            <div className="actions">
+                <button className="btn login-btn" onClick={onLogin}>Login</button>
+                <button className="btn signup-btn" onClick={onSignup}>Sign-up</button>
+            </div>
         </div>
     );
 }
